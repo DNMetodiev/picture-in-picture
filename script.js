6e834d33c5af63fbfb992e2b0fe20ddd7cf82112
DNMetodiev <metodievdenis@yahoo.com>
2024-03-28 17:26:06 +0200

    implement the picture in picture functionality

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-function printName() {
-  return 'Andre'
+const videoElement = document.getElementById('video')
+const button = document.getElementById('button')
+
+
+//Prompt to select media stream, pass to video element, then play 
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play()
+    }
+  } catch (error) {
+    console.log('error', error);
+  }
 }
-function findName() {
-  return printName()
-}
-function sayMyName() {
-  return findName()
-}
-sayMyName()
+
+button.addEventListener('click', async () => {
+  // Disable Button when clicked
+  button.disabled = true;
+  //Start pucture in picture
+  await videoElement.requestPictureInPicture();
+  // Reset the button 
+  button.disabled = false;
+});
+
+//On Load 
+selectMediaStream();
